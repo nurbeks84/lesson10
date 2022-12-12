84f8bee306e5a53a6500b5bbd9ef00060041ddb0
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2022-12-12 19:57:34 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int o = sc.nextInt();
+        int x = 0;
+        while (x < o) {
+            if (x != 0) {
+                System.out.println(x);
+                System.out.println(Math.pow(x, 5));
+            }
+            x += 4;
+
+        }
+    }
+}
